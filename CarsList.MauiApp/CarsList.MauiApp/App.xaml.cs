72f6732911af513b3a83242e79cf203c0ccda1e2
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-08-19 17:38:48 +0530

    Initial Version

---
+++ CarsList.MauiApp/CarsList.MauiApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace CarsList.MauiApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
