00ae34ffb101e6961f57b779d8b770f14b338d4f
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-08-19 18:25:29 +0530

    Changes

---
+++ CarsListApp.Maui/CarsListApp.Maui/ViewModels/BaseViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace CarsListApp.Maui.ViewModels;
+
+public partial class BaseViewModel : INotifyPropertyChanged
+{
+    private string _title;
+    private bool _isBusy;
+
+    public string Title
+    {
+        get => _title;
+
+        set
+        {
+            if (_title != value)
+            {
+                _title = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+
+    public bool IsBusy
+    {
+        get => _isBusy;
+
+        set
+        {
+            if (_isBusy != value)
+            {
+                _isBusy = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    public void OnPropertyChanged([CallerMemberName] string propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
