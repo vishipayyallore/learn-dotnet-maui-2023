55131b8757e6754a77a92b85bc8e66e11966aad9
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-08-19 18:35:27 +0530

    Update BaseViewModel.cs

--- CarsListApp.Maui/CarsListApp.Maui/ViewModels/BaseViewModel.cs
+++ CarsListApp.Maui/CarsListApp.Maui/ViewModels/BaseViewModel.cs
@@ -1,45 +1,59 @@
-﻿using System.ComponentModel;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.ComponentModel;
 using System.Runtime.CompilerServices;
 
 namespace CarsListApp.Maui.ViewModels;
 
-public partial class BaseViewModel : INotifyPropertyChanged
+public partial class BaseViewModel : ObservableObject
 {
-    private string _title;
-    private bool _isBusy;
+    [ObservableProperty]
+    private string title;
 
-    public string Title
-    {
-        get => _title;
+    [ObservableProperty]
+    [NotifyPropertyChangedFor(nameof(IsNotLoading))]
+    private bool isLoading;
 
-        set
-        {
-            if (_title != value)
-            {
-                _title = value;
-                OnPropertyChanged();
-            }
-        }
-    }
-
-    public bool IsBusy
-    {
-        get => _isBusy;
-
-        set
-        {
-            if (_isBusy != value)
-            {
-                _isBusy = value;
-                OnPropertyChanged();
-            }
-        }
-    }
-
-    public event PropertyChangedEventHandler PropertyChanged;
-
-    public void OnPropertyChanged([CallerMemberName] string propertyName = null)
-    {
-        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-    }
+    public bool IsNotLoading => !IsLoading;
 }
+
+
+//public partial class BaseViewModel : INotifyPropertyChanged
+//{
+//    private string _title;
+//    private bool _isBusy;
+
+//    public string Title
+//    {
+//        get => _title;
+
+//        set
+//        {
+//            if (_title != value)
+//            {
+//                _title = value;
+//                OnPropertyChanged();
+//            }
+//        }
+//    }
+
+//    public bool IsBusy
+//    {
+//        get => _isBusy;
+
+//        set
+//        {
+//            if (_isBusy != value)
+//            {
+//                _isBusy = value;
+//                OnPropertyChanged();
+//            }
+//        }
+//    }
+
+//    public event PropertyChangedEventHandler PropertyChanged;
+
+//    public void OnPropertyChanged([CallerMemberName] string propertyName = null)
+//    {
+//        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+//    }
+//}
